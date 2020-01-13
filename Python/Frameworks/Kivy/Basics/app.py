c60bd02a9b46137f34d8791a869a29d55af5eb82
Fardeen Khan <khanfardeen9983@gmail.com>
2020-01-14 00:52:47 +0530

    A simple form in a grid layout : Kivy

--- Python/Frameworks/Kivy/Basics/app.py
+++ Python/Frameworks/Kivy/Basics/app.py
@@ -1,11 +1,46 @@
-import kivy
+# Kivy app
 from kivy.app import App
+
+# UI widgets
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
 
+# Custom GridLayout implementation
+class CustomGrid(GridLayout):
+    # constructor
+    def __init__(self, **kwargs):
+        # Call super class (GridLayout) constructor first
+        super(CustomGrid, self).__init__(**kwargs)
+
+        # Set columns in GridLayout
+        self.cols = 2
+
+        # Adding widgets to our layout
+        self.add_widget(Label(text="First Name : "))
+        # Create local  text input field with single line input
+        self.first_name = TextInput(text='', multiline=False)
+        # Add the text input widget
+        self.add_widget(self.first_name)
+
+        # Create other fields
+        self.add_widget(Label(text="Last Name : "))
+        self.last_name = TextInput(text='', multiline=False)
+        self.add_widget(self.last_name)
+
+        self.add_widget(Label(text="Email : "))
+        self.email = TextInput(text='', multiline=False)
+        self.add_widget(self.email)
+
+        
+
+# The main class that is our entire application
 class MyApp(App):
+    # returns a window that is our Kivy application
     def build(self):
-        return Label(text="Kivy Example")
+        # Return our compliacted widget composition
+        return CustomGrid()
 
 
 if __name__ == '__main__':
