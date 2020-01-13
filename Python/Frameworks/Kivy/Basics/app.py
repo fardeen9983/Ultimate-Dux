0a83e8ef66f2050ca74a8e3c0a6a2dfaeb2e9434
Fardeen Khan <khanfardeen9983@gmail.com>
2020-01-14 01:01:59 +0530

    Adding inner grid to main grid

--- Python/Frameworks/Kivy/Basics/app.py
+++ Python/Frameworks/Kivy/Basics/app.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
 # Custom GridLayout implementation
@@ -13,27 +14,35 @@ class CustomGrid(GridLayout):
     def __init__(self, **kwargs):
         # Call super class (GridLayout) constructor first
         super(CustomGrid, self).__init__(**kwargs)
-
+        self.cols =1        
+        # Create an inner grid layout to house form fields and labels. (As colspan kind of thing is not available)
+        self.inner = GridLayout()
+        
         # Set columns in GridLayout
-        self.cols = 2
+        self.inner.cols = 2
 
         # Adding widgets to our layout
-        self.add_widget(Label(text="First Name : "))
+        self.inner.add_widget(Label(text="First Name : "))
         # Create local  text input field with single line input
         self.first_name = TextInput(text='', multiline=False)
         # Add the text input widget
-        self.add_widget(self.first_name)
+        self.inner.add_widget(self.first_name)
 
         # Create other fields
-        self.add_widget(Label(text="Last Name : "))
+        self.inner.add_widget(Label(text="Last Name : "))
         self.last_name = TextInput(text='', multiline=False)
-        self.add_widget(self.last_name)
+        self.inner.add_widget(self.last_name)
 
-        self.add_widget(Label(text="Email : "))
+        self.inner.add_widget(Label(text="Email : "))
         self.email = TextInput(text='', multiline=False)
-        self.add_widget(self.email)
+        self.inner.add_widget(self.email)
 
-        
+        # Add the inner grid as element of main grid
+        self.add_widget(self.inner)
+
+        # Create a submit button
+        self.submit = Button(text="Submit",font_size=40)
+        self.add_widget(self.submit)
 
 # The main class that is our entire application
 class MyApp(App):
