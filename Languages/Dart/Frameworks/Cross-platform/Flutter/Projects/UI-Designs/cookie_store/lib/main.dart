241739fc3b6d17078dea0b71259bb1261cbc9fb3
Fardeen Khan <khanfardeen9983@gmail.com>
2020-01-27 14:02:26 +0530

    Cookie Store : Starting UI design

---
+++ Languages/Dart/Frameworks/Cross-platform/Flutter/Projects/UI-Designs/cookie_store/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cookie Store',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
