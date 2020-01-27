e0eda17ba8716b77c7629b51f201872883272d07
Fardeen Khan <khanfardeen9983@gmail.com>
2020-01-27 14:21:11 +0530

    Cookie Store : Add assets and theme

--- Languages/Dart/Frameworks/Cross-platform/Flutter/Projects/UI-Designs/cookie_store/lib/main.dart
+++ Languages/Dart/Frameworks/Cross-platform/Flutter/Projects/UI-Designs/cookie_store/lib/main.dart
@@ -7,6 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          textTheme: TextTheme(
+              body1:
+              TextStyle(fontFamily: "Varela", color: Color(0xFF545D68)))),
+      debugShowCheckedModeBanner: false,
       title: 'Cookie Store',
       home: MyHomePage(),
     );
@@ -23,6 +28,51 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.grey,
+              size: 30,
+            ),
+            onPressed: () {}),
+        title: Text(
+          "Pickup",
+          style: TextStyle(
+              color: Theme
+                  .of(context)
+                  .textTheme
+                  .body1
+                  .color,
+              fontFamily: 'Varela',
+              fontSize: 22),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.notifications_none,
+                color: Colors.orange,
+                size: 30,
+              ),
+              onPressed: () {})
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(left: 20),
+        children: <Widget>[
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            "Categories",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 42),
+          )
+        ],
+      ),
+    );
   }
 }
