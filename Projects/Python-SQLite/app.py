12c4a800f63e5f834a87d1a2994211bff3f4a673
Fardeen Khan <khanfardeen9983@gmail.com>
2020-07-28 12:37:43 +0530

    Python - SQLite - Query data

--- Projects/Python-SQLite/app.py
+++ Projects/Python-SQLite/app.py
@@ -22,13 +22,32 @@ c = conn.cursor()
 # c.execute("INSERT INTO customers VALUES('Fardeen', 'Khan', 'ezio@gmail.com')")
 
 # Add multiple records at once
-rows = [
-            ('Pablo','Escobar','pablo@gmail.com'),
-            ('Diablo','Senu','diablo@gmail.com'),
-            ('Boss','Baby','baby@gmail.com')
-       ]
+# rows = [
+#             ('Pablo','Escobar','pablo@gmail.com'),
+#             ('Diablo','Senu','diablo@gmail.com'),
+#             ('Boss','Baby','baby@gmail.com')
+#        ]
 # Execute many inert commands together
-c.executemany("INSERT INTO customers VALUES(?,?,?)",rows)
+# c.executemany("INSERT INTO customers VALUES(?,?,?)",rows) 
+
+# Query data
+# c.execute("SELECT * FROM customers")
+
+# # Returns first record
+# one = c.fetchone()
+# print(one)
+
+# # Returns multiple records
+# many = c.fetchmany(3)
+# print(many)
+
+# # Returns all records
+# all = c.fetchall()
+# print(all)
+
+# Note - keep in mind that running a fetch commond moves the cursor position in the table and already fetched rows are omitted from being fetched again from the same cursor
+
+# Commit your changes
 conn.commit()
 
 # Closing the connection
